fa5a6058b45f044dc783de71ff6128fac49d7bcd
phuochungtr <49616523+phuochungtr@users.noreply.github.com>
2020-06-04 16:06:51 +0700

    checkAttack

--- Assets/Codes/GameState.java
+++ Assets/Codes/GameState.java
@@ -1,6 +1,7 @@
 package Assets.Codes;
 
-import java.awt.Graphics; 
+import java.awt.Graphics;
+import java.util.ArrayList;
 
 public class GameState extends State {
 
@@ -9,32 +10,37 @@ public class GameState extends State {
     private Enemy e_bot;
     private Enemy e_left;
     private Enemy e_right;
+    private ArrayList<Enemy> enemies;
 
     public GameState(Game game){
-       super(game);
-       p = new Player(game,223,160,(short)32,(short)32);
-       e_top = new Enemy(game,215,30,(short)32,(short)32,(short)1);
-       e_bot = new Enemy(game,215,210,(short)32,(short)32,(short)2);
-       e_left = new Enemy(game,35,130,(short)32,(short)32,(short)3);
-       e_right = new Enemy(game,400,130,(short)32,(short)32,(short)4);
-       
-       //e = new Enemy(215,50);
+        super(game);
+        enemies = new ArrayList<Enemy>();
+        p = new Player(game,223,160,(short)32,(short)32);
+        e_top = new Enemy(game,215,30,(short)32,(short)32,(short)1);
+        e_bot = new Enemy(game,215,210,(short)32,(short)32,(short)2);
+        e_left = new Enemy(game,35,130,(short)32,(short)32,(short)3);
+        e_right = new Enemy(game,400,130,(short)32,(short)32,(short)4);
+        enemies.add(e_right);
+        enemies.add(e_left);
+        enemies.add(e_top);
+        enemies.add(e_bot);
     }
 
     public void update(){
-       p.update();
-       e_top.update();
-       e_bot.update();
-       e_left.update();
-       e_right.update();
+        p.update();
+        p.checkAttack(enemies);
+        e_top.update();
+        e_bot.update();
+        e_left.update();
+        e_right.update();
     }
 
     public void paint(Graphics g){
-      g.drawImage(Assets.background, 0, 0, null);
-      p.paint(g);
-      e_top.paint(g);
-      e_bot.paint(g);
-      e_left.paint(g);
-      e_right.paint(g);
+        g.drawImage(Assets.background, 0, 0, null);
+        p.paint(g);
+        e_top.paint(g);
+        e_bot.paint(g);
+        e_left.paint(g);
+        e_right.paint(g);
     }
 }
--- Assets/Codes/Player.java
+++ Assets/Codes/Player.java
@@ -1,12 +1,16 @@
 package Assets.Codes;
 
+import javax.swing.*;
 import java.awt.Graphics;
+import java.util.ArrayList;
 
 public class Player extends Entity {
 
     private Animation a;
     private Game game;
     private boolean isFaceFront, isFaceBack, isFaceRight, isFaceLeft;
+    private int score;
+    private int attackRange = 60;
 
     public static final float DEFAULT_X = 223.0f;
     public static final float DEFAULT_Y = 160.0f;
@@ -20,6 +24,7 @@ public class Player extends Entity {
         isFaceFront = true;
         isFaceLeft = false;
         isFaceRight = false;
+        score = 0;
     }
 
     public void resetPosition(){
@@ -92,5 +97,89 @@ public class Player extends Entity {
 
         g.drawImage(a.getCurrentFrame(), (int) x, (int) y, null);
 
+    }
+
+    public void checkAttack(ArrayList<Enemy> enemies) {
+        for(int i = 0; i < enemies.size(); i++) {
+            Enemy e = enemies.get(i);
+            if(i == 0 ) {
+                if (game.getKeyManager().right) {
+                    if (
+                            e.getX() > x
+                                    && e.getX() < x + attackRange
+                    ) {
+                        score++;
+                        e.setX(400);
+
+                    }
+                }
+                if(e.getX() <= x ){
+                    JOptionPane.showMessageDialog(null,score);
+                    score = 0;
+                    enemies.get(0).setX(400);
+                    enemies.get(1).setX(20);
+                    enemies.get(2).setY(20);
+                    enemies.get(3).setY(230);
+                }
+            }
+            if(i == 1 ) {
+                if (game.getKeyManager().left) {
+                    if (
+                            e.getX() < x
+                                    && e.getX() > x - attackRange
+                    ) {
+                        score++;
+                        e.setX(20);
+                    }
+                }
+                if(e.getX() >= x ){
+                    JOptionPane.showMessageDialog(null,score);
+                    score = 0;
+                    enemies.get(0).setX(400);
+                    enemies.get(1).setX(20);
+                    enemies.get(2).setY(20);
+                    enemies.get(3).setY(230);
+                }
+            }
+            if(i == 2 ) {
+                if (game.getKeyManager().up) {
+                    if (
+                            e.getY() < y
+                                    && e.getY() > y - attackRange
+                    ) {
+                        score++;
+                        e.setY(20);
+                    }
+                }
+                if(e.getY() >= y ){
+                    JOptionPane.showMessageDialog(null,score);
+                    score = 0;
+                    enemies.get(0).setX(400);
+                    enemies.get(1).setX(20);
+                    enemies.get(2).setY(20);
+                    enemies.get(3).setY(230);
+
+                }
+            }
+            if(i == 3 ) {
+                if (game.getKeyManager().down) {
+                    if (
+                            e.getY() > y
+                                    && e.getY() < y + attackRange
+                    ) {
+                        score++;
+                        e.setY(230);
+                    }
+                }
+                if(e.getY() <= y ){
+                    JOptionPane.showMessageDialog(null,score);
+                    score = 0;
+                    enemies.get(0).setX(400);
+                    enemies.get(1).setX(20);
+                    enemies.get(2).setY(20);
+                    enemies.get(3).setY(230);
+                }
+            }
+        }
     }
 }
